380839c2845c4ff129d723f071f5284e5b0228cc
sagnitzuasnabar-pixel <sagnit.zuasnabar@gmail.com>
2026-05-10 19:44:14 -0500

    feat: add static data (FAQs, motivos, nav links)

---
+++ src/data/feautures-data.js
@@ -0,0 +1,31 @@
+import imgTV from '../assets/television.svg'
+import imgDescarga from '../assets/download.svg'
+import imgDonde from '../assets/device-multiple.svg'
+import imgNinos from '../assets/kids.svg'
+
+export const motivos = [
+  {
+    id: 1,
+    titulo: "Disfruta en tu TV",
+    descripcion: "Ve en smart TV, PlayStation, Xbox, Chromecast, Apple TV, reproductores de Blu-ray y más.",
+    imagen: imgTV
+  },
+  {
+    id: 2,
+    titulo: "Descarga tus series para verlas offline",
+    descripcion: "Guarda tu contenido favorito y siempre tendrás algo para ver.",
+    imagen: imgDescarga
+  },
+  {
+    id: 3,
+    titulo: "Disfruta donde quieras",
+    descripcion: "Películas y series ilimitadas en tu teléfono, tablet, laptop y TV.",
+    imagen: imgDonde
+  },
+  {
+    id: 4,
+    titulo: "Crea perfiles para niños",
+    descripcion: "Los niños vivirán aventuras con sus personajes favoritos en un espacio diseñado exclusivamente para ellos, gratis con tu membresía.",
+    imagen: imgNinos
+  }
+]
